84c6c98b0893288f29497b5a4b69b27606a3bf17
W1lDR7 <wjduran79@gmail.com>
2025-02-11 21:22:47 -0400

    Arreglando el merge

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,20 @@
-import { Stack } from "expo-router";
+import { Stack, usePathname, useRouter } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
 
 export default function Layout() {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (pathname === "/") {
+      router.replace("/singup"); // Redirect without showing blank screen
+    }
+  }, [pathname]);
+
   return (
     <>
-      <StatusBar hidden /> {/* Hides the status bar */}
+      <StatusBar hidden /> 
       <Stack screenOptions={{ headerShown: false }} />
     </>
   );
