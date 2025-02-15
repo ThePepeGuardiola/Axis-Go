fd4ff1c0656e0529608ff3f6950bb5e8aa1c9b72
ThePepeGuardiola <acosta.josue.ismael@gmail.com>
2025-02-14 22:44:30 -0400

    Mejora del layout y responsive del apartado
    en este commit me encargue de resolver problemas con el responsive y agregar detalles como el boxshadow del boton iniciar y el fondo svg

--- app/not_founds.tsx
+++ app/not_founds.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link, Stack } from 'expo-router';
 import { StyleSheet } from 'react-native';
-
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
@@ -11,7 +10,7 @@ export default function NotFoundScreen() {
         <Stack.Screen options={{ title: 'Oops!' }} />
         <ThemedView style={styles.container}>
             <ThemedText type="title">This screen doesn't exist.</ThemedText>
-            <Link href="/" style={styles.link}>
+            <Link href="/not_founds" style={styles.link}>
             <ThemedText type="link">Go to home screen!</ThemedText>
             </Link>
         </ThemedView>
