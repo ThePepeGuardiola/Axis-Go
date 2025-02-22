40a14611bb2c458462f30fe6a61463c4b4ba1c2e
W1lDR7 <wjduran79@gmail.com>
2025-02-22 00:57:51 -0400

    fixing

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,16 @@
-// app/index.tsx
-import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View, Image, Pressable, Text } from "react-native";
-import { useRouter } from "expo-router";
 
-export default function LoginScreen() {
-  const router = useRouter();
 
-  return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
+export default function App() {
+    return(
+        <View style={styles.container}>
+            
+            <StatusBar style="auto"/>
 
             {/* IMAGES */}
 
+<<<<<<< HEAD
 
             <Image
                       source={require('@/assets/images/welcome-image.png')}
@@ -23,18 +21,34 @@ export default function LoginScreen() {
                       source={require('@/assets/images/back-image.png')}
                       style={styles.backimage}
                     />
+=======
+>>>>>>> parent of 1e02cac (Dashboard completado)
 
-      {/* TEXT */}
-      <Text style={styles.mtext}>
-        Descubre tu ruta de transporte ideal aquí.
-      </Text>
-      <Text style={styles.stext}>
-        Explora todas las rutas de transporte disponibles según tus intereses
-        y destino principal.
-      </Text>
+            <Image
+                      source={require('@/assets/images/welcome-image.png')}
+                      style={styles.mainimage}
+                    />
+
+<<<<<<< HEAD
+        {/* BUTTONS */}
+
+=======
+            <Image
+                      source={require('@/assets/images/back-image.png')}
+                      style={styles.backimage}
+                    />
+
+            {/* TEXT */}
+
+
+            <Text style={styles.mtext}>Descubre tu ruta de transporte ideal aquí.</Text>
+
+            <Text style={styles.stext}>Explora todas las rutas de transporte disponibles según tus intereses y destino principal.</Text>
+
 
         {/* BUTTONS */}
 
+>>>>>>> parent of 1e02cac (Dashboard completado)
 
         <View style={styles.logbuttons}>
                <Pressable style={styles.button}>
@@ -53,78 +67,79 @@ export default function LoginScreen() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    overflow: "hidden",
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "flex-start",
-    justifyContent: "flex-end",
-  },
+    container: {
+        overflow: 'hidden',
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'flex-start',
+        justifyContent: 'flex-end',
+    },
 
-  backimage: {
-    zIndex: -1,
-    position: "absolute",
-    top: 0,
-    right: 0,
-  },
+    backimage: {
+        zIndex: -1,
+        position: 'absolute',
+        top: 0,
+        right: 0,
+    },
 
-  mainimage: {
-    overflow: "hidden",
-    left: -330,
-    position: "absolute",
-    bottom: 50,
-    width: 1040,
-    resizeMode: "contain",
-  },
+    mainimage: {
+        overflow: 'hidden',
+        left: -330,
+        position: 'absolute',
+        bottom: 50,
+        width: 1040,
+        resizeMode: 'contain',
+    },
 
-  mtext: {
-    fontSize: 36,
-    position: "absolute",
-    paddingBottom: 330,
-    color: "#900020",
-    fontWeight: "bold",
-    textAlign: "center",
-  },
+    mtext: {
+        fontSize: 36,
+        position: 'absolute',
+        paddingBottom: 330,
+        color: '#900020',
+        fontWeight: 'bold',
+        textAlign: 'center'
+    },
 
-  stext: {
-    fontSize: 18,
-    textAlign: "center",
-  },
+    stext: {
+        fontSize: 18,
+        textAlign: 'center'
+    },
 
-  logbuttons: {
-    flexDirection: "row",
-  },
+    logbuttons:{
+        flexDirection: 'row',
+    },
 
-  logtext: {
-    fontSize: 25,
-    fontWeight: "bold",
-    color: "white",
-  },
+    logtext: {
+        fontSize: 25,
+        fontWeight: 'bold',
+        color: 'white',
+    },
 
-  button: {
-    width: "70%",
-    marginVertical: 80,
-    marginHorizontal: 40,
-    paddingVertical: 20,
-    borderRadius: 10,
-    alignItems: "center",
-    backgroundColor: "#900020",
-    shadowColor: "#FF9EB4",
-    shadowOffset: { width: 0, height: 7 },
-    shadowOpacity: 1,
-    shadowRadius: 10,
-  },
+    button: {
+        width: '70%',
+        marginVertical: 80,
+        marginHorizontal: 40,
+        paddingVertical: 20,
+        borderRadius: 10,
+        alignItems: 'center',
+        backgroundColor: '#900020',
+        shadowColor: '#FF9EB4',
+        shadowOffset:{ width: 0, height: 7},
+        shadowOpacity: 1,
+        shadowRadius: 10,
+    },
 
-  logtext2: {
-    fontSize: 25,
-    fontWeight: "bold",
-  },
+    logtext2: {
+        fontSize: 25,
+        fontWeight: 'bold',
+    },
 
-  button2: {
-    width: "60%",
-    marginVertical: 80,
-    paddingVertical: 20,
-    borderRadius: 10,
-    alignItems: "center",
-  },
+    button2: {
+        width: '60%',
+        marginVertical: 80,
+        paddingVertical: 20,
+        borderRadius: 10,
+        alignItems: 'center',
+    },
+
 });
