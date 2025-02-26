47b4abdaca3bbe48bc1230a7d06c70b57f2d673b
edwinpe123 <21-0484@copphu.unphu.edu.do>
2025-02-25 22:37:20 -0400

    termine el historial

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,457 +1,127 @@
 import { StatusBar } from "expo-status-bar";
-import FontAwesome from "@expo/vector-icons/FontAwesome";
-import AntDesign from '@expo/vector-icons/AntDesign';
-import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons';
-import { StyleSheet, View, Text } from "react-native";
-
-
-
+import { StyleSheet, View, Image, Pressable, Text } from "react-native";
 
 
 export default function App() {
     return(
-      
         <View style={styles.container}>
+            
+            <StatusBar style="auto"/>
 
-               <StatusBar style="auto"/>
+            {/* IMAGES */}
 
-               <View style={styles.searchbar}>
 
-               <AntDesign style={styles.left} name="left" size={23} color="black"/>
-                 <View style={styles.search}>
-                     <AntDesign name="search1" size={17} color="#A0A0A0"/>
-                    <Text style={styles.textchbar}> Search </Text>
-                 </View>
-                     <SimpleLineIcons style={styles.bell} name="bell" size={20} color="black" />
-               </View>
-               
-            <Text style={styles.title}>Favoritos</Text>
-               <view style={styles.textcont}>
+            <Image
+                      source={require('@/assets/images/welcome-image.png')}
+                      style={styles.mainimage}
+                    />
 
-                <View style={styles.firstcont}>
+            <Image
+                      source={require('@/assets/images/back-image.png')}
+                      style={styles.backimage}
+                    />
 
-                <FontAwesome name='star'  size={20} color='#900020'/>
-                 < text style={styles.text1}>Supermercados</text> 
-                 < text style={styles.subtext1}>Jumbo, AV. Luperon</text>
-                 < text style={styles.sidetext1}>1.7 Km</text> 
-                 </View>
+            {/* TEXT */}
 
-                 
-                 
-                 <View style={styles.sectcont}> 
-                  
-                 <FontAwesome name='star'  size={20} color='#900020'/>
-                 < text style={styles.text2}>Supermercados</text> 
-                 < text style={styles.subtext2}>Jumbo, AV. Luperon</text>
-                 < text style={styles.sidetext2}>1.7 Km</text> 
-                 </View>
 
-                 <View style={styles.thirdcont}>
+            <Text style={styles.mtext}>Descubre tu ruta de transporte ideal aquí.</Text>
 
-                 <FontAwesome name='star'  size={20} color='#900020'/>
-                 < text style={styles.text3}>Supermercados</text> 
-                 < text style={styles.subtext3}>Jumbo, AV. Luperon</text>
-                 < text style={styles.sidetext3}>1.7 Km</text> 
-                 </View>
+            <Text style={styles.stext}>Explora todas las rutas de transporte disponibles según tus intereses y destino principal.</Text>
 
-                 <View style={styles.fourthtcont}>
 
-                 <FontAwesome name='star'  size={20} color='#900020'/>
-                 < text style={styles.text4}>Supermercados</text>
-                 < text style={styles.subtext4}>Jumbo, AV. Luperon</text>
-                 < text style={styles.sidetext4}>1.7 Km</text>
-                 </View>
+        {/* BUTTONS */}
 
-                 <View style={styles.fifthcont}>
 
-                 <FontAwesome name='star'  size={20} color='#900020'/>
-                 < text style={styles.text5}>Supermercados</text>
-                 < text style={styles.subtext5}>Jumbo, AV. Luperon</text>
-                 < text style={styles.sidetext5}>1.7 Km</text>
-                 </View>
+        <View style={styles.logbuttons}>
+               <Pressable style={styles.button}>
+                       <Text style={styles.logtext}>Login</Text>
+               </Pressable>
 
-                 <View style={styles.sixcont}>
 
-                 <FontAwesome name='star'  size={20} color='#900020'/>
-                 < text style={styles.text6}>Supermercados</text>
-                 < text style={styles.subtext6}>Jumbo, AV. Luperon</text>
-                 < text style={styles.sidetext6}>1.7 Km</text>
-                 </View>
-
-                 <View style={styles.sevencont}>
-
-                 <FontAwesome name='star'  size={20} color='#900020'/>
-                 < text style={styles.text7}>Supermercados</text>
-                 < text style={styles.subtext7}>Jumbo, AV. Luperon</text>
-                 < text style={styles.sidetext7}>1.7 Km</text>
-                 </View>
-
-                 <View style={styles.eightcont}>
-              
-                 <FontAwesome name='star'  size={20} color='#900020'/>
-                 < text style={styles.text8}>Supermercados</text> 
-                 < text style={styles.subtext8}>Jumbo, AV. Luperon</text>
-                 < text style={styles.sidetext8}>1.7 Km</text>
-                 </View>
-
-                 <View style={styles.ninecont}>
-
-                 <FontAwesome name='star'  size={20} color='#900020'/>
-                 < text style={styles.text9}>Supermercados</text> 
-                 < text style={styles.subtext9}>Jumbo, AV. Luperon</text>
-                 < text style={styles.sidetext9}>1.7 Km</text> 
-                 </View>
-
-                 <View style={styles.tencont}>
-
-                 <FontAwesome name='star'  size={20} color='#900020'/>
-                 < text style={styles.text10}>Supermercados</text> 
-                 < text style={styles.subtext10}>Jumbo, AV. Luperon</text>
-                 < text style={styles.sidetext10}>1.7 Km</text> 
-                 </View>
-
-                
-                
-
-               </view>
-               
+               <Pressable style={styles.button2}>
+                       <Text style={styles.logtext2}>Register</Text>
+               </Pressable>
         </View>
-        
+            
+
+     </View>
  );
 }
 
 const styles = StyleSheet.create({
-  
-  container: {
-    height: '100%',
-    overflow: 'hidden',
-    backgroundColor: '#FFC1CF',
-},
+    container: {
+        overflow: 'hidden',
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'flex-start',
+        justifyContent: 'flex-end',
+    },
 
-   searchbar: {
-    backgroundColor: '#ffff',
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    padding: 10,
-    marginBottom: 55,
-   },
+    backimage: {
+        zIndex: -1,
+        position: 'absolute',
+        top: 0,
+        right: 0,
+    },
 
-   search: {
-    position: 'absolute',
-    flexDirection: 'row',
-    backgroundColor: '#EFEFEF',
-    paddingRight: 180,
-    paddingLeft: 10,
-    paddingTop: 5,
-    paddingBottom: 5,
-    marginLeft: 50,
-    marginRight: 50,
-   },
+    mainimage: {
+        overflow: 'hidden',
+        left: -330,
+        position: 'absolute',
+        bottom: 50,
+        width: 1040,
+        resizeMode: 'contain',
+    },
 
-   textchbar: {
-    color: '#A0A0A0',
-    fontSize: 16,
-    paddingLeft: 5,
-   },
+    mtext: {
+        fontSize: 36,
+        position: 'absolute',
+        paddingBottom: 330,
+        color: '#900020',
+        fontWeight: 'bold',
+        textAlign: 'center'
+    },
 
-   left: {
-    top: 5,
-   },
+    stext: {
+        fontSize: 18,
+        textAlign: 'center'
+    },
 
-   bell: {
-    top: 5,
-    right: 5,
-   },
+    logbuttons:{
+        flexDirection: 'row',
+    },
 
-   title: {
-    paddingTop: 100,
-    position: 'absolute',
-    marginLeft: 15,
-    fontSize: 30,
-    color: '#3B3B3B',
-},
+    logtext: {
+        fontSize: 25,
+        fontWeight: 'bold',
+        color: 'white',
+    },
 
-   textcont: {
-    position: 'relative',
-    color: '#000000',
-    marginTop: 57,
-    paddingBottom: 50,
-    marginLeft: 15,
-},
+    button: {
+        width: '70%',
+        marginVertical: 80,
+        marginHorizontal: 40,
+        paddingVertical: 20,
+        borderRadius: 10,
+        alignItems: 'center',
+        backgroundColor: '#900020',
+        shadowColor: '#FF9EB4',
+        shadowOffset:{ width: 0, height: 7},
+        shadowOpacity: 1,
+        shadowRadius: 10,
+    },
 
+    logtext2: {
+        fontSize: 25,
+        fontWeight: 'bold',
+    },
 
-// 1
-
-
-firstcont: {
-  paddingBottom: 20,
-},
-
-   text1: {
-    position: 'absolute',
-    fontSize: 20,
-    paddingLeft: 50,
-},
-
-   subtext1:{
-    paddingTop: 5,
-    fontSize: 15,
-    color: '#8F8F8F',
-    paddingLeft: 50,
-},
-
-  sidetext1: {
-    fontSize: 15,
-    color: '#8F8F8F',
-    position: 'absolute',
-    marginTop: 26,
-    marginLeft: 280,
-  },
-
-  // 2
-
-  sectcont: {
-    paddingBottom: 20,
-  },
-  
-  text2: {
-    position: 'absolute',
-    fontSize: 20,
-    paddingLeft: 50,
-  },
-  
-  subtext2:{
-    paddingTop: 5,
-    fontSize: 15,
-    color: '#8F8F8F',
-    paddingLeft: 50,
-  },
-  
-  sidetext2: {
-    fontSize: 15,
-    color: '#8F8F8F',
-    position: 'absolute',
-    marginTop: 26,
-    marginLeft: 280,
-  },
-
-  // 3
-
-  thirdcont: {
-    paddingBottom: 20,
-  },
-  
-  text3: {
-    position: 'absolute',
-    fontSize: 20,
-    paddingLeft: 50,
-  },
-  
-  subtext3:{
-    paddingTop: 5,
-    fontSize: 15,
-    color: '#8F8F8F',
-    paddingLeft: 50,
-  },
-  
-  sidetext3: {
-    fontSize: 15,
-    color: '#8F8F8F',
-    position: 'absolute',
-    marginTop: 26,
-    marginLeft: 280,
-  },
-
-  // 4
-
-  fourthtcont: {
-    paddingBottom: 20,
-  },
-  
-  text4: {
-    position: 'absolute',
-    fontSize: 20,
-    paddingLeft: 50,
-  },
-  
-  subtext4:{
-    paddingTop: 5,
-    fontSize: 15,
-    color: '#8F8F8F',
-    paddingLeft: 50,
-  },
-  
-  sidetext4: {
-    fontSize: 15,
-    color: '#8F8F8F',
-    position: 'absolute',
-    marginTop: 26,
-    marginLeft: 280,
-  },
-
-  // 5
-
-  fifthcont: {
-    paddingBottom: 20,
-  },
-  
-  text5: {
-    position: 'absolute',
-    fontSize: 20,
-    paddingLeft: 50,
-  },
-  
-  subtext5:{
-    paddingTop: 5,
-    fontSize: 15,
-    color: '#8F8F8F',
-    paddingLeft: 50,
-  },
-  
-  sidetext5: {
-    fontSize: 15,
-    color: '#8F8F8F',
-    position: 'absolute',
-    marginTop: 26,
-    marginLeft: 280,
-  },
-
-  // 6
-
-  sixcont: {
-    paddingBottom: 20,
-  },
-  
-  text6: {
-    position: 'absolute',
-    fontSize: 20,
-    paddingLeft: 50,
-  },
-  
-  subtext6:{
-    paddingTop: 5,
-    fontSize: 15,
-    color: '#8F8F8F',
-    paddingLeft: 50,
-  },
-  
-  sidetext6: {
-    fontSize: 15,
-    color: '#8F8F8F',
-    position: 'absolute',
-    marginTop: 26,
-    marginLeft: 280,
-  },
-
-  // 7
-
-  sevencont: {
-    paddingBottom: 20,
-  },
-  
-  text7: {
-    position: 'absolute',
-    fontSize: 20,
-    paddingLeft: 50,
-  },
-  
-  subtext7:{
-    paddingTop: 5,
-    fontSize: 15,
-    color: '#8F8F8F',
-    paddingLeft: 50,
-  },
-  
-  sidetext7: {
-    fontSize: 15,
-    color: '#8F8F8F',
-    position: 'absolute',
-    marginTop: 26,
-    marginLeft: 280,
-  },
-
-  // 8
-
-  eightcont: {
-    paddingBottom: 20,
-  },
-  
-  text8: {
-    position: 'absolute',
-    fontSize: 20,
-    paddingLeft: 50,
-  },
-  
-  subtext8:{
-    paddingTop: 5,
-    fontSize: 15,
-    color: '#8F8F8F',
-    paddingLeft: 50,
-  },
-  
-  sidetext8: {
-    fontSize: 15,
-    color: '#8F8F8F',
-    position: 'absolute',
-    marginTop: 26,
-    marginLeft: 280,
-  },
-
-  // 9
-
-  ninecont: {
-    paddingBottom: 20,
-  },
-  
-  text9: {
-    position: 'absolute',
-    fontSize: 20,
-    paddingLeft: 50,
-  },
-  
-  subtext9:{
-    paddingTop: 5,
-    fontSize: 15,
-    color: '#8F8F8F',
-    paddingLeft: 50,
-  },
-  
-  sidetext9: {
-    fontSize: 15,
-    color: '#8F8F8F',
-    position: 'absolute',
-    marginTop: 26,
-    marginLeft: 280,
-  },
-
-  // 10
-
-  tencont: {
-    paddingBottom: 20,
-  },
-  
-  text10: {
-    position: 'absolute',
-    fontSize: 20,
-    paddingLeft: 50,
-  },
-  
-  subtext10:{
-    paddingTop: 5,
-    fontSize: 15,
-    color: '#8F8F8F',
-    paddingLeft: 50,
-  },
-  
-  sidetext10: {
-    fontSize: 15,
-    color: '#8F8F8F',
-    position: 'absolute',
-    marginTop: 26,
-    marginLeft: 280,
-  },
-
+    button2: {
+        width: '60%',
+        marginVertical: 80,
+        paddingVertical: 20,
+        borderRadius: 10,
+        alignItems: 'center',
+    },
 
 });
-
---
+++ app/(tabs)/list.tsx
@@ -0,0 +1,181 @@
+import { StatusBar } from "expo-status-bar";
+import  FontAwesome from '@expo/vector-icons/FontAwesome';
+import { StyleSheet, SectionList, FlatList, View, Text } from "react-native";
+
+
+const DATA = [
+  {
+    id: "1",
+    category: "Supermercados",
+    icon: "star",
+    items1: ["Jumbo, Av. Luperon"],
+    items2: ["1.7 Km"],
+  },
+  {
+    id: "2",
+    category: "Gym",
+    icon: "star",
+    items1: ["Smart fit, Nuñez de caceres"],
+    items2: ["1.5 Km",],
+  },
+  {
+    id: "2",
+    category: "Gym",
+    icon: "star",
+    items1: ["Smart fit, Nuñez de caceres"],
+    items2: ["1.5 Km",],
+  },
+  {
+    id: "2",
+    category: "Gym",
+    icon: "star",
+    items1: ["Smart fit, Nuñez de caceres"],
+    items2: ["1.5 Km",],
+  },
+  {
+    id: "2",
+    category: "Gym",
+    icon: "star",
+    items1: ["Smart fit, Nuñez de caceres"],
+    items2: ["1.5 Km",],
+  },
+  {
+    id: "2",
+    category: "Gym",
+    icon: "star",
+    items1: ["Smart fit, Nuñez de caceres"],
+    items2: ["1.5 Km",],
+  },
+  {
+    id: "2",
+    category: "Gym",
+    icon: "star",
+    items1: ["Smart fit, Nuñez de caceres"],
+    items2: ["1.5 Km",],
+  },
+  {
+    id: "2",
+    category: "Gym",
+    icon: "star",
+    items1: ["Smart fit, Nuñez de caceres"],
+    items2: ["1.5 Km",],
+  },
+  {
+    id: "2",
+    category: "Gym",
+    icon: "star",
+    items1: ["Smart fit, Nuñez de caceres"],
+    items2: ["1.5 Km",],
+  },
+  {
+    id: "2",
+    category: "Gym",
+    icon: "star",
+    items1: ["Smart fit, Nuñez de caceres"],
+    items2: ["1.5 Km",],
+  },
+];
+
+
+export default function App() {
+    return(
+      
+        <View style={styles.container}>
+
+               <StatusBar style="auto"/>
+
+               
+
+
+            <Text style={styles.title}>Favoritos</Text>
+
+
+            <FlatList
+      data={DATA}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => (
+        <View style={styles.rowContainer}>
+          {/* Columna 1: Categoría con Icono */}
+          <View style={styles.headerContainer}>
+            <FontAwesome name={item.icon} size={23} color="#900020" style={styles.icon} />
+            <Text style={styles.category}>{item.category}</Text>
+          </View>
+
+          {/* Contenedor de Sublistas */}
+          <View style={styles.subListsContainer}>
+            {/* Sublista 1 */}
+            <FlatList
+              data={item.items1}
+              keyExtractor={(subItem, index) => subItem + index}
+              renderItem={({ item: subItem }) => (
+                <Text style={styles.subItem}>{subItem}</Text>
+              )}
+            />
+
+            {/* Sublista 2 */}
+            <FlatList
+              data={item.items2}
+              keyExtractor={(subItem, index) => subItem + index}
+              renderItem={({ item: subItem }) => (
+                <Text style={styles.subItem}>{subItem}</Text>
+              )}
+            />
+          </View>
+        </View>
+      )}
+    />
+               
+               
+        </View>
+        
+ );
+}
+
+const styles = StyleSheet.create({
+  
+  container: {
+    height: '100%',
+    overflow: 'hidden',
+    backgroundColor: '#FFC1CF',
+},
+
+title: {
+  marginLeft: 15,
+  marginBottom: 20,
+  fontSize: 30,
+  color: '#3B3B3B',
+},
+
+rowContainer: {
+  justifyContent: "space-between",
+  alignItems: "flex-start",
+  padding: 15,
+},
+
+headerContainer: {
+  flexDirection: "row",
+  alignItems: "center",
+  width: "30%",
+},
+
+icon: {
+  marginRight: 20,
+},
+
+category: {
+  fontSize: 18,
+},
+subListsContainer: {
+  flexDirection: "row", 
+  justifyContent: "space-between",
+  width: "105%", 
+},
+subItem: {
+  fontSize: 15,
+  color: '#8F8F8F',
+  paddingVertical: 2,
+  paddingHorizontal: 37,
+},
+
+});
+
