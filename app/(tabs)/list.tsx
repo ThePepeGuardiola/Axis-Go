dd1381b38b770d380b88f952cb9a428aa12aabaa
edwinpe123 <21-0484@copphu.unphu.edu.do>
2025-02-26 23:15:25 -0400

    termime el historial

--- app/(tabs)/list.tsx
+++ app/(tabs)/list.tsx
@@ -1,7 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import  FontAwesome from '@expo/vector-icons/FontAwesome';
-import { StyleSheet, SectionList, FlatList, View, Text } from "react-native";
+import { StyleSheet, FlatList, View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import * as Font from "expo-font";
 
+// lista
 
 const DATA = [
   {
@@ -11,8 +14,6 @@ const DATA = [
     items1: ["Jumbo, Av. Luperon"],
     items2: ["1.7 Km"],
   },
-
-  
   {
     id: "2",
     category: "Gym",
@@ -78,48 +79,91 @@ const DATA = [
   },
 ];
 
+// Fuente del texto 
 
 export default function App() {
+
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    async function loadFonts() {
+      await Font.loadAsync({
+        "Roboto-Regular": require("../../assets/fonts/Roboto-Regular.ttf"), 
+      });
+      setFontsLoaded(true);
+    }
+    loadFonts();
+  }, []);
+
+  if (!fontsLoaded) {
+    return <Text>Cargando fuentes...</Text>;
+  }
+
+  // Degradado del Background
+  
+    const gradientHeight = 700;
+    const gradientBackground = "#FFC1CF";
+    const data = Array.from({ length: gradientHeight }, (_, i) => i);
     return(
+
       
         <View style={styles.container}>
+          
+          {data.map((_, i) => (
+          <View
+            key={i}
+            style={{
+              position: "absolute",
+              backgroundColor: gradientBackground,
+              height: 5,
+              top:  (gradientHeight - i - 1) ,
+              right: 0,
+              left: 0,
+              opacity: (0.6 / gradientHeight) * (i + 1),
+            }}
+          />
+        ))}
 
+    
                <StatusBar style="auto"/>
-
-               
-
 
             <Text style={styles.title}>Favoritos</Text>
 
-
             <FlatList
+
       data={DATA}
       keyExtractor={(item) => item.id}
       renderItem={({ item }) => (
-        <View style={styles.rowContainer}>
-          {/* Columna 1: Categoría con Icono */}
+        <View style={styles.listContainer}>
+
+          {/* Icono */}
+
           <View style={styles.headerContainer}>
             <FontAwesome name={item.icon} size={23} color="#900020" style={styles.icon} />
-            <Text style={styles.category}>{item.category}</Text>
+            <Text style={styles.MainText}>{item.category}</Text>
           </View>
 
           {/* Contenedor de Sublistas */}
+
           <View style={styles.subListsContainer}>
+
             {/* Sublista 1 */}
+
             <FlatList
               data={item.items1}
               keyExtractor={(subItem, index) => subItem + index}
               renderItem={({ item: subItem }) => (
-                <Text style={styles.subItem}>{subItem}</Text>
+                <Text style={styles.subText}>{subItem}</Text>
               )}
             />
 
             {/* Sublista 2 */}
+
             <FlatList
               data={item.items2}
               keyExtractor={(subItem, index) => subItem + index}
               renderItem={({ item: subItem }) => (
-                <Text style={styles.subItem}>{subItem}</Text>
+                <Text style={styles.subText}>{subItem}</Text>
               )}
             />
           </View>
@@ -128,27 +172,30 @@ export default function App() {
     />
                
                
-        </View>
-        
- );
+  </View>
+  
+);
 }
+
 
 const styles = StyleSheet.create({
   
   container: {
+    flex: 1,
     height: '100%',
     overflow: 'hidden',
-    backgroundColor: '#FFC1CF',
 },
 
 title: {
   marginLeft: 15,
-  marginBottom: 20,
-  fontSize: 30,
+  marginBottom: 10,
+  marginTop: 20,
+  fontSize: 28,
   color: '#3B3B3B',
+  fontFamily: "Roboto-Regular",
 },
 
-rowContainer: {
+listContainer: {
   justifyContent: "space-between",
   alignItems: "flex-start",
   padding: 15,
@@ -164,7 +211,7 @@ icon: {
   marginRight: 20,
 },
 
-category: {
+MainText: {
   fontSize: 18,
 },
 subListsContainer: {
@@ -172,7 +219,7 @@ subListsContainer: {
   justifyContent: "space-between",
   width: "105%", 
 },
-subItem: {
+subText: {
   fontSize: 15,
   color: '#8F8F8F',
   paddingVertical: 2,
